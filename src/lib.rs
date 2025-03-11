31982d7003d97a26b187f96ceea0ea39a73af2c9
rctrinity <upfront-boxing-0j@icloud.com>
2025-03-11 17:15:42 -0500

    Documentation Update
    - Completed documenting all helper functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 // lib.rs
 
 // Publicly expose the functions at the crate root
+/// This crate stores all the data structures and implementations for JSON returned data.
 pub mod models;
+/// This crate stores globally related variabes and helper functions.
 pub mod utils;
+/// This crate stores all the data structures and implementations for app configuration (e.g. RPC).
 pub mod config;
+/// This crate stores all the RPC function calls to Bitcoin node.
 pub mod rpc;
+/// This crate stores all  the functions to display the TUI dashboard.
 pub mod display;
